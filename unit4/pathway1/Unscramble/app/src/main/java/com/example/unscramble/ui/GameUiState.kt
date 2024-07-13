d413fc288407441a09a1582d6007677245d539cb
AnhTuan1678 <21020397@vnu.edu.vn>
2024-07-13 11:11:13 +0700

    Unscramble pathway1 unit 4

--- unit4/pathway1/Unscramble/app/src/main/java/com/example/unscramble/ui/GameUiState.kt
+++ unit4/pathway1/Unscramble/app/src/main/java/com/example/unscramble/ui/GameUiState.kt
@@ -1,3 +1,13 @@
 package com.example.unscramble.ui
 
-data class GameUiState()
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+data class GameUiState(
+    val currentScrambledWord: String = "",
+    val isGuessedWordWrong: Boolean = false,
+    val currentWordCount: Int = 1,
+    val score: Int = 0,
+    var isGameOver : Boolean = false
+)
