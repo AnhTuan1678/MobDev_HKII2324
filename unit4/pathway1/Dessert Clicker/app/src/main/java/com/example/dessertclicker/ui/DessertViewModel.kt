0cfba2395b7eb05e7d77390c5c59181ff0722098
AnhTuan1678 <21020397@vnu.edu.vn>
2024-07-13 12:16:45 +0700

    hoàn thành pathway1 unit 4

--- unit4/pathway1/Dessert Clicker/app/src/main/java/com/example/dessertclicker/ui/DessertViewModel.kt
+++ unit4/pathway1/Dessert Clicker/app/src/main/java/com/example/dessertclicker/ui/DessertViewModel.kt
@@ -1,4 +1,37 @@
 package com.example.dessertclicker.ui
 
-class DessertViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.dessertclicker.data.Datasource.dessertList
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class DessertViewModel: ViewModel() {
+    private val _dessertUiState = MutableStateFlow(DessertUiState())
+    val dessertUiState = _dessertUiState.asStateFlow()
+
+    fun onDessertClicked() {
+        _dessertUiState.update { uiState ->
+            val sold =  uiState.dessertsSold + 1
+            uiState.copy(
+                revenue = uiState.revenue + uiState.currentDessertPrice,
+                dessertsSold = sold,
+                currentDessertIndex = determineDessert(sold),
+                currentDessertImageId = dessertList[uiState.currentDessertIndex].imageId,
+                currentDessertPrice = dessertList[uiState.currentDessertIndex].price,
+            )
+        }
+    }
+
+    private fun determineDessert(dessertsSold: Int): Int{
+        var dessert = 0
+        for(i in dessertList.indices){
+            if(dessertsSold >= dessertList[i].startProductionAmount){
+                dessert = i
+            }else{
+                break
+            }
+        }
+        return  dessert
+    }
 }
